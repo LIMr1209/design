867cb0a698fc44027aa8131e8b8b079b9f9684f3
LIMr1209 <aaa1058169464@126.com>
2019-02-25 18:30:22 +0800

    jd_ymx

--- design/design/spiders/amazon.py
+++ design/design/spiders/amazon.py
@@ -1,40 +1,38 @@
 # 亚马逊
-
 import scrapy
 import os
 import requests
 import time
 import random
 from design.settings import USER_AGENTS
-from design.items import ProductItem
+
 
 class AmazonSpider(scrapy.spiders.Spider):
     name = "amazon"
     allowed_domains = ["amazon.cn"]
     key_word = "打印机"
-    cookie = {"x-wl-uid":"1HLv1HxJzEtQLlBYWMfv7puk8/voXH/VIgoiQ9ewAWWntrB+0QJz17S/Yi9k9IVEHfJxncjXErfE=","session-id":"459-1170388-1651454","ubid-acbcn":"459-7032326-5051849","session-token":"'ZJr/OPdrRIP7Tg8VLRvOkiU68zMtoU5eJ0VNVhwpkZYpwsoOp78gmf78w3me5drvIPjhR0vKW21oGo/SttpKHZ9YWIIowhPkk9y5Wx2wDOvLg8wjCtSfM9v4jikU1g7/9w2Gsl+nrPvTdOMsvUY2cxJ8HEdA50qFVn/3xpxquzX5nd4NU3qu+9ekVv9N8pyRcDojYHFKEuGkrKRRYrTRclwT/G1lMsJ8ESuXsXGfumdB758Mm1RNfA=='","i18n-prefs":"CNY","session-id-time":"2082787201l","csm-hit":"tb:11Z75V7KVZRH53JGJTV8+sa-11Z75V7KVZRH53JGJTV8-9TCZAX86FDRJFQG2VEK4|1550628429630&t:1550628429630&adb:adblk_no"}
+    cookie = {"x-wl-uid": "1HLv1HxJzEtQLlBYWMfv7puk8/voXH/VIgoiQ9ewAWWntrB+0QJz17S/Yi9k9IVEHfJxncjXErfE=",
+              "session-id": "459-1170388-1651454", "ubid-acbcn": "459-7032326-5051849",
+              "session-token": "'ZJr/OPdrRIP7Tg8VLRvOkiU68zMtoU5eJ0VNVhwpkZYpwsoOp78gmf78w3me5drvIPjhR0vKW21oGo/SttpKHZ9YWIIowhPkk9y5Wx2wDOvLg8wjCtSfM9v4jikU1g7/9w2Gsl+nrPvTdOMsvUY2cxJ8HEdA50qFVn/3xpxquzX5nd4NU3qu+9ekVv9N8pyRcDojYHFKEuGkrKRRYrTRclwT/G1lMsJ8ESuXsXGfumdB758Mm1RNfA=='",
+              "i18n-prefs": "CNY", "session-id-time": "2082787201l",
+              "csm-hit": "tb:11Z75V7KVZRH53JGJTV8+sa-11Z75V7KVZRH53JGJTV8-9TCZAX86FDRJFQG2VEK4|1550628429630&t:1550628429630&adb:adblk_no"}
 
     def start_requests(self):
         headers = {
-            "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "user-agent": random.choice(USER_AGENTS)
         }
         url = "https://www.amazon.cn/s/ref=sr_pg_%d?rh=i:aps,k:%s&page=%d&keywords=%s&ie=UTF8&qid=%d"
-        for page in range(1,21):
+        for page in range(1, 21):
             t = int(time.time())
-            yield scrapy.Request(url=url % (page ,self.key_word ,page ,self.key_word ,t), callback=self.list_page ,cookies=self.cookie,headers=headers)
-            # break
+            yield scrapy.Request(url=url % (page, self.key_word, page, self.key_word, t), callback=self.list_page,
+                                 cookies=self.cookie, headers=headers)
 
     def list_page(self, response):
-        # item = ProductItem()
-        # item['evt'] = 7
-        # item['channel'] = 'amazon'
-        # item['tags'] = '家用、办公,电器,空气加湿器'
         img_urls = response.xpath('//a[@class="a-link-normal a-text-normal"]/img/@src').extract()
+
         ################################保存图片 start
         for img_url in img_urls:
-            # item['title'] = response.xpath("//img[@src='" + str(img_url) + "']/@alt").extract()[0].strip().replace('(美国品牌 保税仓发货 包邮包税)', '')
-            # item['url'] = response.xpath("//img[@src='" + str(img_url) + "']/parent::a/@href").extract()[0].strip()
             img_url = img_url.replace('AA200', 'SX679')
             try:
                 path = './image_test/' + self.key_word
@@ -53,10 +51,3 @@ class AmazonSpider(scrapy.spiders.Spider):
                     print('保存图片失败')
             except:
                 print('访问图片失败')
-            # item['img_urls'] = ','.join([img_url])
-            # yield item
-            # break
-        ################################保存图片 end
-        # print(tmp_urls)
-        # yield item
-            # break
--- design/design/spiders/jd.py
+++ design/design/spiders/jd.py
@@ -2,12 +2,10 @@
 import scrapy
 from selenium import webdriver
 import time
-from design.items import ProductItem
 import os
 import requests
 import random
-from urllib.parse import unquote
-import re
+
 
 class JdSpider(scrapy.spiders.Spider):
     name = "jd"
@@ -15,31 +13,24 @@ class JdSpider(scrapy.spiders.Spider):
     start_urls = [
         "https://search.jd.com"
     ]
-    cookies = {"__jdu":"1504540579","xtest":"8417.cf6b6759","shshshfpa":"809881f3-e0b7-a978-e43d-c273ef43feba-1547618014","shshshfpb":"a1YprPpOwUxdo4mm8gl4Z6Q%3D%3D","qrsc":"3","user-key":"b66a81b7-5bf0-46c0-978a-9a720bd8cecc","cn":"0","mt_xid":"V2_52007VwMWU1hcV1sbTxFsBWFWEgINXltGSk4QDBliA0cCQVFbWBtVTl9QYwQaUQ0MW10deRpdBW8fE1FBWFdLH0gSXwdsABFiX2hSah1NHVUBZgsXV21YV1wY","_gcl_au":"1.1.1305404444.1550111031","unpl":"V2_ZzNtbUtfQUJxAU4EK0kPDGJWQF4RAkMVc19AAHweVQA3BkJeclRCFX0UR1dnGVUUZwYZWUNcQBJFCEdkeBBVAWMDE1VGZxBFLV0CFSNGF1wjU00zQwBBQHcJFF0uSgwDYgcaDhFTQEJ2XBVQL0oMDDdRFAhyZ0AVRQhHZHsdXQBjBhNZRF9zJXI4dmR%2fH1QAbwoiXHJWc1chVE5dehFfSGcHE1hGUkIRcwB2VUsa","CCC_SE":"ADC_zaPEmyTs5ShlPGdnwf49YCNXcSLezlrSejtd8ZkWINX4xFev5mLDKQDK6wpOnjdUMU8f4tUIWS772Sq7IXjmV15Gq8UmoKdAMSCxqa%2bchyKKWP7B1wn4FZkM7y4uOt9nZ%2fm1NgPJeEBlkUpzbSkX7rqbN4qzCX%2fqCxBga4qeMvbQGIY%2f048Phg9TDCkynfkfo1IR4Tlb2DjQQPWBsvVH1bkkeOLM81OiGp4RFHKCLRVUfj%2bfxJ9%2fqjpRYlPlf58wBkrcewAYPFUMWQGSsraN2qVmJ9BiOXYNtgsbpQriVUUeBzlSeh0iIID4o4J81d10d5KsLxhqLOwS1rIpB0FxepzBSUg2od9xVP8cX0coiynwZgr5pnTwgmHJS2nwwieexheCkH3pUr1Mj6ypYi3N6uCfD9Ketn09EUNSuumFGerAXPQXnQa86Vwhqkx1bTMx1yz%2biL7kBkrLbcStHpT3IXyplcprKvxeUPVOxVgnoiCSYAxAmtcvk95XA0gWZdXoUTbfuqsNdihIag%2f823y7PU1xGMXJcvjAoEhvABoi9tntdRqo0YLI7CmrW2pVJxPfQSTMDjebrlaN5jrJvm8bnRAo7oTuJES4xMC2Vvc1i1XdkqJuoLnVvY%2fyykoOns1E2D3QzKEyz0cJyfB0oj%2f7UmLCZ9u0DJRux%2fvaUWhFhWt3piHscCzUUMlaouiSqfbYIWtHSz938m6zGnjUJYV31e1m%2fQ8fxQ711AmmLSWH9Xw%3d","__jda":"122270672.1504540579.1547547744.1550472608.1550472643.16","__jdc":"122270672","__jdv":"122270672|baidu-pinzhuan|t_288551095_baidupinzhuan|cpc|0f3d30c8dba7459bb52f2eb5eba8ac7d_0_893f589aaab84dc2bd117f7e6684a4a2|1550472642940","ipLoc-djd":"53283-53309-0-0","rkv":"V0500","shshshfp":"54799f70e6b1c62917854d5ef6c209ed","3AB9D23F7A4B3C9B":"4VNI4Z2IPWV5NDI3RK77JJJZ53DLOSPHC4FSLCLXHRH5WCL7NNCFXQIDPKSN2EB6ZWZ7VQUTVPQTIXGD2TXMSDXRL4","__jdb":"122270672.7.1504540579|16.1550472643","shshshsID":"1538781fbcd6daa2ca4759c596f01eb9_5_1550473754874"}
     key_words = "热水器"
-    
+
     def start_requests(self):
         browser = webdriver.Chrome()
-        for i in range(1,8):
-            browser.get("https://search.jd.com/Search?keyword=%s&wq=%s&page=%d&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&stock=1&s=61&click=0" %(self.key_words, self.key_words ,i))
+        for i in range(1, 8):
+            browser.get(
+                "https://search.jd.com/Search?keyword=%s&wq=%s&page=%d&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&stock=1&s=61&click=0" % (
+                    self.key_words, self.key_words, i))
             urls = browser.find_elements_by_xpath('//div[@class="p-img"]/a[@target="_blank"]')
             for url in urls:
                 yield scrapy.Request(url.get_attribute('href'), callback=self.parse)
-            # break
 
     def parse(self, response):
         print('*' * 10)
-        # item = ProductItem()
-        # item['url'] = response.url
-        # item['title'] = response.xpath('//div[@class="sku-name"]/text()').extract()[0].strip().replace('/n','')
-        # item['evt'] = 7
-        # item['channel'] = 'jd_search'
-        tmp_urls = response.xpath('//div[@id="spec-list"]/ul/li/img/@data-url').extract()
-        img_urls = []
-        for tmp_url in tmp_urls:
-            img_urls.append("http://img10.360buyimg.com/n0/%s" % tmp_url)
-        # item['img_urls'] = ','.join(img_urls)
-        ################################保存图片 start
+        img_urls = response.xpath('//div[@id="spec-list"]/ul/li/img/@data-url').extract()
+        for i in range(len(img_urls)):
+            img_urls[i] = 'http://img10.360buyimg.com/n0/%s' % img_urls[i]
+        print(img_urls)
         for img_url in img_urls:
             try:
                 path = './image_test/' + self.key_words
@@ -58,19 +49,3 @@ class JdSpider(scrapy.spiders.Spider):
                     print('保存图片失败')
             except:
                 print('访问图片失败')
-        ################################保存图片 end
-
-        # tags = response.xpath('//div[@id="crumb-wrap"]/descendant::a[@clstag]/text()').extract()[0:3]
-        # item['tags'] = ','.join(tags)
-        # item['brand_tags'] = response.xpath('//ul[@id="parameter-brand"]/li/a/text()').extract()[0]
-        #
-        # price = response.xpath('//div[@class="dd"]/span[@class="p-price"]/span[2]/text()').extract()
-        # if price is not None:
-        #     try :
-        #         item['price'] = price[0]
-        #         item['currency_type'] = 1
-        #     except IndexError:
-        #         pass
-        # print(item)
-        # yield item
-        
