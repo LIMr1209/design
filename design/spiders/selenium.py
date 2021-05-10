c0d48ea57e37bab4e54dee9937bafaaaea32ee98
LIMr1209 <aaa1058169464@126.com>
2021-05-10 13:56:43 +0800

    selenium

--- design/spiders/selenium.py
+++ design/spiders/selenium.py
@@ -6,6 +6,7 @@ import scrapy
 from scrapy.utils.project import get_project_settings
 from pydispatch import dispatcher
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from scrapy import signals
@@ -126,9 +127,16 @@ class SeleniumSpider(scrapy.Spider):
         #
         # chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\selenium\AutomationProfile"
         chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:{}".format(kwargs['se_port']))
-
+        # 其中PageLoadStrategy有三种选择： 默认normal
+        # (1) NONE: 当html下载完成之后，不等待解析完成，selenium会直接返回
+        # (2) EAGER: 要等待整个dom树加载完成，即DOMContentLoaded这个事件完成，仅对html的内容进行下载解析
+        # (3) NORMAL: 即正常情况下，selenium会等待整个界面加载完成（指对html和子资源的下载与解析,如JS文件，图片等，不包括ajax）
+        caps = DesiredCapabilities().CHROME
+        # caps["pageLoadStrategy"] = "normal"  # complete
+        caps["pageLoadStrategy"] = "eager"  #interactive
+        # caps["pageLoadStrategy"] = "none"
         # 初始化chrome对象
-        self.browser = webdriver.Chrome(options=chrome_options)
+        self.browser = webdriver.Chrome(options=chrome_options, desired_capabilities=caps)
 
         # self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         #     "source": """
@@ -141,7 +149,7 @@ class SeleniumSpider(scrapy.Spider):
         # if self.windowHeight and self.windowWidth:
         #     self.browser.set_window_size(900, 900)
         self.browser.set_page_load_timeout(kwargs['time_out'])  # 页面加载超时时间
-        self.browser.set_script_timeout(10) # 执行js 超时时间
+        self.browser.set_script_timeout(kwargs['time_out']) # 执行js 超时时间
         self.wait = WebDriverWait(self.browser, 30)  # 指定元素加载超时时间
         # 设置信号量，当收到spider_closed信号时，调用mySpiderCloseHandle方法，关闭chrome
         # dispatcher.connect(receiver=self.mySpiderCloseHandle,
